17771476f78f74ac00fdd8b65d6e288b60dafeba
Reynelle <Rtalford13@gmail.com>
2021-05-26 21:27:57 -0400

    created models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :airlines
+  resources :flights
   get 'bookings/index'
   get 'bookings/new'
   get 'bookings/edit'
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 resources :users 
+resources :flights
+resources :airlines
 end
