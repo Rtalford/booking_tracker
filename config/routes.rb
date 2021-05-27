23a294a1a8d12deae741d6ad154a5abccead9508
Reynelle <Rtalford13@gmail.com>
2021-05-26 21:09:06 -0400

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/delete'
-  get 'users/show'
+
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/edit'
+  get 'bookings/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 resources :users 
